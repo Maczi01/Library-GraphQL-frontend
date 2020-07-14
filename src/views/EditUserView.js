a6508cfb604497ad7c11933a46d242cbd9dcf760
Maczi01 <mateusz.w.twardy@gmail.com>
2020-07-15 00:01:24 +0200

    create edit user view

--- src/views/EditUserView.js
+++ src/views/EditUserView.js
@@ -1,5 +1,24 @@
 import React from "react";
+import {useParams} from "react-router";
+import {useQuery} from "@apollo/client";
+import {CircularProgress} from "@chakra-ui/core";
+import {GET_USER_QUERY} from "./UserDetailsView";
 
 export default function EditUserPage() {
-    return <p>Edit user page</p>;
+
+    const {userId} = useParams();
+    const {loading, error, data} = useQuery(GET_USER_QUERY, {
+        variables: {userId}
+    });
+    if (loading) {
+        return <CircularProgress isIndeterminate color="green" my="20%"></CircularProgress>;
+    }
+    if (error) {
+        return <p>Could not load user"</p>;
+    }
+    const {user} = data;
+    if(!user){
+        return <p>User not found</p>
+    }
+    return <p>Edit user({user.name}) page</p>;
 }
--- src/views/UserDetailsView.js
+++ src/views/UserDetailsView.js
@@ -8,6 +8,11 @@ import {BOOK_COPY_PARTS_FRAGMENT} from "../components/BookCopy/fragments";
 import Flex from "@chakra-ui/core/dist/Flex";
 import Heading from "@chakra-ui/core/dist/Heading";
 import BorrowRandomButton from "../components/BookCopy/BorrowRandomButton";
+import AdminActions from "../components/AdminActions";
+import ButtonLink from "../components/ButtonLink";
+import UserDeleteButton from "../components/UserDeleteButton";
+import Stack from "@chakra-ui/core/dist/Stack";
+import ResetDataButton from "../components/ResetDataButton";
 
 
 export const GET_USER_QUERY = gql`
@@ -40,6 +45,10 @@ export default function UserDetailsPage() {
         return <p>Could not load user"</p>;
     }
     const {user} = data;
+    if(!user){
+        return <p>User not found</p>
+    }
+
     return (
         <Box>
             <UserDetails user={user}/>
@@ -56,7 +65,11 @@ export default function UserDetailsPage() {
                     />
                 ))}
             </Flex>
-
+            <AdminActions direction="column">
+                <ButtonLink to={`/users/${user.id}/edit`}> Edit user</ButtonLink>
+                <UserDeleteButton userId={user.id}/>
+                <ResetDataButton/>
+            </AdminActions>
             <Heading as="h3" size="lg" textAlign="center"> Borrowed books </Heading>
             <Flex wrap="wrap" width="60vw">
                 {user.borrowedBookCopies.map(bookCopy => (
