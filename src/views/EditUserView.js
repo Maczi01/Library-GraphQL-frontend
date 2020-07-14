7b07c8884ba1cf8052034ea4ceb13fc06884ac53
Maczi01 <mateusz.w.twardy@gmail.com>
2020-07-15 00:10:41 +0200

    changes in code (edit)

--- src/views/EditUserView.js
+++ src/views/EditUserView.js
@@ -1,11 +1,15 @@
 import React from "react";
-import {useParams} from "react-router";
+import {useNavigate, useParams} from "react-router";
 import {useQuery} from "@apollo/client";
 import {CircularProgress} from "@chakra-ui/core";
 import {GET_USER_QUERY} from "./UserDetailsView";
+import UserCreateForm from "../components/UserCreateForm";
+import {useToast} from "../components/Toast";
+import UserUpdateForm from "../components/UserUpdateForm";
 
 export default function EditUserPage() {
-
+    const toast = useToast();
+    const navigate = useNavigate();
     const {userId} = useParams();
     const {loading, error, data} = useQuery(GET_USER_QUERY, {
         variables: {userId}
@@ -17,8 +21,14 @@ export default function EditUserPage() {
         return <p>Could not load user"</p>;
     }
     const {user} = data;
-    if(!user){
+    if (!user) {
         return <p>User not found</p>
     }
-    return <p>Edit user({user.name}) page</p>;
+
+    return <UserUpdateForm
+        user={user}
+        onUpdate={() => {
+            toast({status: "warning", description: "Not implemented!"})
+        }} onCancel={() => navigate(`/users/${user.id}`)}
+        isUpdating={false}/>
 }
