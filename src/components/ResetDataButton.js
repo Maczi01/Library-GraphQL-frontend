16f2b1ad68de0a4f5ae7bdd43ce95baf389240ed
Maczi01 <mateusz.w.twardy@gmail.com>
2020-07-25 14:21:50 +0200

    add reset mutation

--- src/components/ResetDataButton.js
+++ src/components/ResetDataButton.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import {Button} from "@chakra-ui/core";
 import {useToast} from "./Toast";
+import {gql} from "@apollo/client";
+
+
+const RESET_DATA_MUTATION = gql`
+    mutation ResetData{
+        resetData{
+            success
+            message
+        }
+    }
+    
+`
 
 export default function ResetDataButton(props) {
     const toast = useToast();
