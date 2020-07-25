1457b25e6a3508d87ea6e7c4c8005a8bd08e3c07
Maczi01 <mateusz.w.twardy@gmail.com>
2020-07-26 00:33:32 +0200

    first version of delete user button

--- src/components/UserDeleteButton.js
+++ src/components/UserDeleteButton.js
@@ -1,16 +1,37 @@
 import React from 'react'
 import {Button} from "@chakra-ui/core";
 import {useToast} from "./Toast";
+import {gql, useMutation} from "@apollo/client";
 
-export default function UserDeleteButton(props) {
-    const toast = useToast();
-    return <Button
-        onClick={() =>
-            toast({status: "warning", description: "Not implemented!"})
+
+const DELETE_USER_MUTATION = gql`
+    mutation DeleteUser($userId: ID!){
+        deleteUser(id: $userId){
+            success
+            message
+            id
         }
-        isLoading={false}
-        {...props}
-    >
-        Delete User
-    </Button>
+    }
+
+`
+export default function UserDeleteButton({userId, ...remainingProps}) {
+    const toast = useToast();
+    const [deleteUser, { loading }] = useMutation(DELETE_USER_MUTATION, {
+        onCompleted: ({ deleteUser: { success, message } }) => {
+            toast({
+                description: message,
+                status: success ? "success" : "error"
+            });
+        }
+    });
+    return (
+        <Button onClick={() =>
+            deleteUser({
+                variables: {userId}
+            })
+        } isLoading={loading} {...remainingProps}
+        >
+            Delete user!
+        </Button>
+    );
 }
