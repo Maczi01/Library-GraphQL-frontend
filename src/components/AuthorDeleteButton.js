a7b12c273ff2af0d96cd6a41bc5354b91ae3d4fb
Maczi01 <mateusz.w.twardy@gmail.com>
2020-07-27 00:26:32 +0200

    add button for delete author

--- src/components/AuthorDeleteButton.js
+++ src/components/AuthorDeleteButton.js
@@ -1,16 +1,73 @@
 import React from 'react'
 import {Button} from "@chakra-ui/core";
 import {useToast} from "./Toast";
+import {gql, useMutation} from "@apollo/client";
+import {USER_DETAILS_PARTS_FRAGMENT} from "./UserDetails";
+import {GET_USER_QUERY} from "../views/UserDetailsView";
+import {AUTHOR_DETAILS_PARTS_FRAGMENT} from "./AuthorDetails";
+import {GET_AUTHOR_QUERY} from "../views/AuthorDetailsView";
 
-export default function AuthorDeleteButton(props) {
-    const toast = useToast();
-    return <Button
-        onClick={() =>
-            toast({status: "warning", description: "Not implemented!"})
+const DELETE_AUTHOR_MUTATION = gql`
+    mutation DeleteAuthor($authorId: ID!){
+        deleteUser(id: $authorId){
+            success
+            message
+            id
         }
-        isLoading={false}
-        {...props}
-    >
-        Delete Author
-    </Button>
+    }
+`
+
+export default function AuthorDeleteButton({authorId, ...remainingProps}) {
+    const toast = useToast();
+    const [deleteAuthor, { loading }] = useMutation(DELETE_AUTHOR_MUTATION, {
+        variables: { authorId },
+        onCompleted: ({ deleteAuthor: { success, message, id } }) => {
+            toast({
+                description: message,
+                status: success ? "success" : "error"
+            });
+        },
+        update: cache => {
+            const cachedData = cache.readQuery({
+                query: gql`
+                    query GetAllAuthors($searchQuery: String) {
+                        authors(searchQuery: $searchQuery) {
+                            ...authorDetailsParts
+                        }
+                    }
+                    ${AUTHOR_DETAILS_PARTS_FRAGMENT}
+                `,
+                variables: { searchQuery: "" }
+            });
+            cache.writeQuery({
+                query: gql`
+                    query GetAllAuthors($searchQuery: String) {
+                        authors(searchQuery: $searchQuery) {
+                            ...authorDetailsParts
+                        }
+                    }
+                    ${AUTHOR_DETAILS_PARTS_FRAGMENT}
+                `,
+                variables: { searchQuery: "" },
+                data: {
+                    authors: cachedData.authors.filter(author => author.id !== authorId)
+                }
+            });
+            cache.writeQuery({
+                query: GET_AUTHOR_QUERY,
+                variables: { authorId },
+                data: {
+                    user: null
+                }
+            });
+        }
+    });
+    return (
+        <Button onClick={() =>
+            deleteAuthor()
+        } isLoading={loading} {...remainingProps}
+        >
+            Delete Author!
+        </Button>
+    );
 }
