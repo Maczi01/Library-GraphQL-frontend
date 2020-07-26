3e37df44df6f42bbd90e2a44b6e737526b3dc4de
Maczi01 <mateusz.w.twardy@gmail.com>
2020-07-26 23:01:24 +0200

    work with user delete button

--- src/components/UserDeleteButton.js
+++ src/components/UserDeleteButton.js
@@ -2,7 +2,8 @@ import React from 'react'
 import {Button} from "@chakra-ui/core";
 import {useToast} from "./Toast";
 import {gql, useMutation} from "@apollo/client";
-
+import ALL_USERS_QUERY from '../views/UsersView'
+import {GET_USER_QUERY} from "../views/UserDetailsView";
 
 const DELETE_USER_MUTATION = gql`
     mutation DeleteUser($userId: ID!){
@@ -12,16 +13,47 @@ const DELETE_USER_MUTATION = gql`
             id
         }
     }
-
 `
 export default function UserDeleteButton({userId, ...remainingProps}) {
     const toast = useToast();
-    const [deleteUser, { loading }] = useMutation(DELETE_USER_MUTATION, {
-        onCompleted: ({ deleteUser: { success, message } }) => {
+    const [deleteUser, {loading}] = useMutation(DELETE_USER_MUTATION, {
+        onCompleted: ({deleteUser: {success, message}}) => {
             toast({
                 description: message,
                 status: success ? "success" : "error"
             });
+        },
+        update: (cache) => {
+            const cachedData = cache.readQuery(
+                {
+                    query: ALL_USERS_QUERY,
+                    variables: {
+                        searchQuery: ""
+                    }
+                }
+            );
+            cache.writeQuery(
+                {
+                    query: ALL_USERS_QUERY,
+                    variables: {
+                        searchQuery: ""
+                    },
+                    data: {
+                       users:  cachedData.users.filter(user => user.id !== userId)
+                    }
+                }
+            );
+            cache.writeQuery(
+                {
+                    query: GET_USER_QUERY,
+                    variables: {
+                        userId
+                    },
+                    data: {
+                        user:  null
+                    }
+                }
+            )
         }
     });
     return (
